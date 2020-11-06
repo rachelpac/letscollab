2d2240d339d4169d9af665b7583725878dc99e54
rachelpac <rachel.pac@hotmail.com>
2020-11-06 15:57:32 +1000

    updated sessions

--- client/js/script.js
+++ client/js/script.js
@@ -1,6 +1,6 @@
 M.AutoInit();
 
-// LOADER 
+// LOADER CHANGED
 
 var loader = '<div class="preloader-wrapper small active"><div class="spinner-layer spinner-blue-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>';
 
